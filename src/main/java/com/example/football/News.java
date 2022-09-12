c61611af5ab62f4141a6492dc6cb138e6f5791a0
susmita19045 <susmitapaul22123@gmail.com>
2022-09-12 22:55:29 +0600

    Add changes

--- src/main/java/com/example/football/News.java
+++ src/main/java/com/example/football/News.java
@@ -21,18 +21,18 @@ public class News {
     private Button Logout;
     @FXML
     private Button Back;
-    @FXML
-    private Hyperlink hyperlink1;
-    @FXML
-    private Hyperlink hyperlink2;
-    @FXML
-    private Hyperlink hyperlink3;
-    @FXML
-    private Hyperlink hyperlink4;
-    @FXML
-    private Hyperlink hyperlink5;
-    @FXML
-    private Hyperlink hyperlink6;
+   // @FXML
+   // private Hyperlink hyperlink1;
+    //@FXML
+    //private Hyperlink hyperlink2;
+    //@FXML
+    //private Hyperlink hyperlink3;
+    //@FXML
+    //private Hyperlink hyperlink4;
+    //@FXML
+    //private Hyperlink hyperlink5;
+   // @FXML
+    //private Hyperlink hyperlink6;
 
 
 
@@ -55,38 +55,39 @@ public class News {
     @FXML
     void openLink1(ActionEvent event) throws URISyntaxException, IOException {
         System.out.println("link clicked!");
-        Desktop Desktop = null;
-        Desktop.getDesktop().browse(new URI("http://eurasiantimes.com/first-eurofighter-typhoon-handed-over-to-qatar-will-guard/"));
+        //Desktop Desktop = null;
+        Desktop.getDesktop().browse(new URI("http://goggle.com/"));
     }
-    @FXML
-    void openLink2(ActionEvent event) throws URISyntaxException, IOException {
-        System.out.println("link clicked!");
-       Desktop Desktop = null;
-        Desktop.getDesktop().browse(new URI("https://www.thedailystar.net/sports/football/news/brazil-play-ghana-tunisia-pre-world-cup-friendlies-3099046"));
-    }
-    @FXML
-    void openLink3(ActionEvent event) throws URISyntaxException, IOException {
-        System.out.println("link clicked!");
-        Desktop Desktop = null;
-        Desktop.getDesktop().browse(new URI("https://bdnews24.com/sport/flwlpigcow"));
-    }
-    @FXML
-    void openLink4(ActionEvent event) throws URISyntaxException, IOException {
-        System.out.println("link clicked!");
-        Desktop Desktop = null;
-        Desktop.getDesktop().browse(new URI("https://www.theguardian.com/sport/the-nightwatchman/2022/aug/19/joy-falling-love-farm-italy-england-cricket-world-cup"));
-    }
-    @FXML
-    void openLink5(ActionEvent event) throws URISyntaxException, IOException {
-        System.out.println("link clicked!");
-        Desktop Desktop = null;
-        Desktop.getDesktop().browse(new URI("https://www.football-espana.net/2022/08/18/sevilla-forward-suffers-devastating-injury-and-ends-world-cup-hopes"));
-    }
-    @FXML
-    void openLink6(ActionEvent event) throws URISyntaxException, IOException {
-        System.out.println("link clicked!");
-        Desktop Desktop = null;
-        Desktop.getDesktop().browse(new URI("https://thepeninsulaqatar.com/article/20/08/2022/fifa-releases-second-official-soundtrack-for-qatar-2022"));
-    }
+//    @FXML
+//    void openLink2(ActionEvent event) throws URISyntaxException, IOException {
+//        System.out.println("link clicked!");
+//       Desktop Desktop = null;
+//        Desktop.getDesktop().browse(new URI("https://www.thedailystar.net/sports/football/news/brazil-play-ghana-tunisia-pre-world-cup-friendlies-3099046"));
+//    }
+//    @FXML
+//    void openLink3(ActionEvent event) throws URISyntaxException, IOException {
+//        System.out.println("link clicked!");
+//        Desktop Desktop = null;
+//        Desktop.getDesktop().browse(new URI("https://bdnews24.com/sport/flwlpigcow"));
+//    }
+//    @FXML
+//    void openLink4(ActionEvent event) throws URISyntaxException, IOException {
+//        System.out.println("link clicked!");
+//        Desktop Desktop = null;
+//        Desktop.getDesktop().browse(new URI("https://www.theguardian.com/sport/the-nightwatchman/2022/aug/19/joy-falling-love-farm-italy-england-cricket-world-cup"));
+//    }
+//    @FXML
+//    void openLink5(ActionEvent event) throws URISyntaxException, IOException {
+//        System.out.println("link clicked!");
+//        Desktop Desktop = null;
+//        Desktop.getDesktop().browse(new URI("https://www.football-espana.net/2022/08/18/sevilla-forward-suffers-devastating-injury-and-ends-world-cup-hopes"));
+//    }
+//    @FXML
+//    void openLink6(ActionEvent event) throws URISyntaxException, IOException {
+//        System.out.println("link clicked!");
+//        Desktop Desktop = null;
+//        Desktop.getDesktop().browse(new URI("https://thepeninsulaqatar.com/article/20/08/2022/fifa-releases-second-official-soundtrack-for-qatar-2022"));
+//
+//      }
 
 }
